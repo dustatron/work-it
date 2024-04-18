5ca0a3c46fefd5c0679398f7c2775822f16072cd
Dusty McCord <dustymccord@gmail.com>
2024-04-17 19:54:08 -0700

    Fix sorting issue

--- src/components/ExerciseListSortable/ExerciseListSortable.tsx
+++ src/components/ExerciseListSortable/ExerciseListSortable.tsx
@@ -1,37 +1,51 @@
-import { Reorder } from "framer-motion"
-import { useState } from "react"
-import { api } from "~/utils/api"
-import { ExerciseInWorkout } from "~/utils/types"
-import ExerciseCard from "./ExerciseCard"
-import { Stack } from "@chakra-ui/react"
+import { Reorder } from "framer-motion";
+import { useState } from "react";
+import { api } from "~/utils/api";
+import { ExerciseInWorkout } from "~/utils/types";
+import ExerciseCard from "./ExerciseCard";
+import { Stack } from "@chakra-ui/react";
 
 type Props = {
-    workoutItems: ExerciseInWorkout[]
-}
+  workoutItems: ExerciseInWorkout[];
+};
 
 export default function ExerciseListSortable({ workoutItems }: Props) {
-    const { mutate } = api.workout.updateSortOrder.useMutation()
-    const intialList = workoutItems.sort((a, b) => a.sortOrder - b.sortOrder)
-    const [exericeList, setExericeList] = useState(intialList)
+  const { mutate } = api.workout.updateSortOrder.useMutation();
+  const intialList = workoutItems.sort((a, b) => a.sortOrder - b.sortOrder);
+  const [exericeList, setExericeList] = useState(intialList);
 
+  const handleNewSort = (sort: ExerciseInWorkout[]) => {
+    const newOrder = sort.map((item, index) => ({
+      id: item?.id,
+      sortOrder: `${index}`,
+    }));
+    mutate({ newOrder });
+    console.log(sort);
+  };
 
-    const handleNewSort = (sort: ExerciseInWorkout[]) => {
-        const newOrder = exericeList.map((item, index) => ({ id: item?.id, sortOrder: `${index}` }))
-        mutate({ newOrder })
-        console.log(sort)
-    }
-
-
-    return (
-        <Reorder.Group axis="y" values={exericeList} onReorder={(order) => { setExericeList(order); handleNewSort(order) }}>
-            <Stack>
-
-                {exericeList.map((item) => (
-                    <Reorder.Item key={item.id} value={item} style={{ listStyle: "none" }}>
-                        <span> <ExerciseCard exercise={item.exercise} /></span>
-                    </Reorder.Item>
-                ))}
-            </Stack>
-        </Reorder.Group>
-    )
+  return (
+    <Reorder.Group
+      axis="y"
+      values={exericeList}
+      onReorder={(order) => {
+        setExericeList(order);
+        handleNewSort(order);
+      }}
+    >
+      <Stack>
+        {exericeList.map((item) => (
+          <Reorder.Item
+            key={item.id}
+            value={item}
+            style={{ listStyle: "none" }}
+          >
+            <span>
+              {" "}
+              <ExerciseCard exercise={item.exercise} />
+            </span>
+          </Reorder.Item>
+        ))}
+      </Stack>
+    </Reorder.Group>
+  );
 }
