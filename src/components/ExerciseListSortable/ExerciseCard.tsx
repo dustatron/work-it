e78a4ce763b4dc7ba2c93ec6f57d90ea0db121d1
Dusty McCord <dustymccord@gmail.com>
2024-04-24 20:42:24 -0700

    Update drag box

--- src/components/ExerciseListSortable/ExerciseCard.tsx
+++ src/components/ExerciseListSortable/ExerciseCard.tsx
@@ -1,4 +1,4 @@
-import { AddIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { AddIcon, ChevronDownIcon, DragHandleIcon } from "@chakra-ui/icons";
 import {
   Text,
   Stack,
@@ -15,11 +15,12 @@ import {
   MenuItem,
   Tag,
 } from "@chakra-ui/react";
-import { Exercise } from "@prisma/client";
+import { Reorder, useDragControls } from "framer-motion";
 import { useState } from "react";
+import { ExerciseInWorkout } from "~/utils/types";
 
 type Props = {
-  exercise: Exercise;
+  exercise: ExerciseInWorkout;
 };
 
 export default function ExerciseCard({ exercise }: Props) {
@@ -29,128 +30,155 @@ export default function ExerciseCard({ exercise }: Props) {
   const [units, setUnits] = useState(0);
   const [sets, setSets] = useState(0);
 
+  const dragControls = useDragControls();
+
+  const item = exercise.exercise;
+
   return (
-    <Card p={1} border="1px" borderColor="gray.300">
-      <Stack direction="row" w="100%">
-        <Stack w="100%" p="0">
-          <Stack direction="row" justifyContent="space-between" w="100%" p="2">
-            <Text
-              fontWeight="bold"
-              textTransform="capitalize"
-              alignContent="center"
-            >
-              {exercise.name}
-            </Text>
-            <Stack direction="row">
-              <Tag variant="subtle" colorScheme="cyan" size="md" px="3">
-                # {sets}
-              </Tag>
-              <Box display={{ base: "inherit", md: "none" }}>
-                <Button name="log" onClick={() => setSets(sets + 1)} px="3">
-                  <Icon display="block" as={AddIcon} h="40px" />
-                </Button>
-              </Box>
-            </Stack>
-          </Stack>
-          <Stack
-            direction={{ base: "column", sm: "column", md: "row" }}
-            justifyContent={{ base: "center", md: "space-between" }}
-          >
+    <Reorder.Item
+      key={exercise.id}
+      value={exercise}
+      style={{ listStyle: "none" }}
+      dragListener={false}
+      dragControls={dragControls}
+    >
+      <Card p={1} border="1px" borderColor="gray.300">
+        <Stack direction="row" w="100%">
+          <Stack w="100%" p="0">
             <Stack
               direction="row"
-              justifyContent={{ base: "center", md: "start" }}
+              justifyContent="space-between"
+              w="100%"
+              p="2"
             >
-              <Box w={{ base: "50%", md: "40%" }}>
-                <FormControl>
-                  <Stack direction="row" spacing="1">
-                    <Input
-                      name="reps"
-                      type="number"
-                      value={reps}
-                      onChange={(e) => setReps(Number(e.target.value))}
-                    />
-                    <Box>
-                      <Menu>
-                        <MenuButton
-                          as={Button}
-                          aria-label="Options"
-                          rightIcon={<ChevronDownIcon />}
-                          variant="outline"
-                        >
-                          {repTitle}{" "}
-                        </MenuButton>
-                        <MenuList>
-                          <MenuItem onClick={() => setRepTitle("Reps")}>
-                            Reps
-                          </MenuItem>
-                          <MenuItem onClick={() => setRepTitle("Min")}>
-                            Min
-                          </MenuItem>
-                          <MenuItem onClick={() => setRepTitle("Sec")}>
-                            Sec
-                          </MenuItem>
-                          <MenuItem onClick={() => setRepTitle("Laps")}>
-                            Laps
-                          </MenuItem>
-                        </MenuList>
-                      </Menu>
-                    </Box>
-                  </Stack>
-                </FormControl>
-              </Box>
-              <Box w={{ base: "50%", md: "45%" }}>
-                <FormControl>
-                  {/* <FormLabel>{unitTitle}</FormLabel> */}
-                  <Stack direction="row" alignItems="center" spacing="1">
-                    <Input
-                      name="units"
-                      type="number"
-                      value={units}
-                      onChange={(e) => setUnits(Number(e.target.value))}
-                    />
-                    <Box>
-                      <Menu>
-                        <MenuButton
-                          as={Button}
-                          rightIcon={<ChevronDownIcon />}
-                          variant="outline"
-                        >
-                          {unitTitle}
-                        </MenuButton>
-                        <MenuList>
-                          <MenuItem onClick={() => setUnitTitle("lbs")}>
-                            lbs
-                          </MenuItem>
-                          <MenuItem onClick={() => setUnitTitle("Intesity")}>
-                            Intesity
-                          </MenuItem>
-                          <MenuItem onClick={() => setUnitTitle("None")}>
-                            None
-                          </MenuItem>
-                        </MenuList>
-                      </Menu>
-                    </Box>
-                  </Stack>
-                </FormControl>
-              </Box>
+              <Stack
+                direction="row"
+                spacing={1}
+                cursor="pointer"
+                onPointerDown={(e) => dragControls.start(e)}
+              >
+                <Flex alignItems="center">
+                  <Icon as={DragHandleIcon} />
+                </Flex>
+                <Text
+                  fontWeight="bold"
+                  textTransform="capitalize"
+                  alignContent="center"
+                >
+                  {item.name}
+                </Text>
+              </Stack>
+              <Stack direction="row">
+                <Tag variant="subtle" colorScheme="cyan" size="md" px="3">
+                  # {sets}
+                </Tag>
+                <Box display={{ base: "inherit", md: "none" }}>
+                  <Button name="log" onClick={() => setSets(sets + 1)} px="3">
+                    <Icon display="block" as={AddIcon} h="40px" />
+                  </Button>
+                </Box>
+              </Stack>
             </Stack>
-            <Flex
-              h="100%"
-              w={{ base: "100%", sm: "100%", md: "auto" }}
-              alignItems={{ base: "center", sm: "center", md: "end" }}
-              justifyContent={{ base: "center", md: "end" }}
-              px="2"
+            <Stack
+              direction={{ base: "column", sm: "column", md: "row" }}
+              justifyContent={{ base: "center", md: "space-between" }}
             >
-              <Box display={{ base: "none", md: "inherit" }}>
-                <Button name="log" onClick={() => setSets(sets + 1)} py="2">
-                  <Icon display="block" as={AddIcon} h="40px" />
-                </Button>
-              </Box>
-            </Flex>
+              <Stack
+                direction="row"
+                justifyContent={{ base: "center", md: "start" }}
+              >
+                <Box w={{ base: "50%", md: "40%" }}>
+                  <FormControl>
+                    <Stack direction="row" spacing="1">
+                      <Input
+                        name="reps"
+                        type="number"
+                        value={reps}
+                        onChange={(e) => setReps(Number(e.target.value))}
+                      />
+                      <Box>
+                        <Menu>
+                          <MenuButton
+                            as={Button}
+                            aria-label="Options"
+                            rightIcon={<ChevronDownIcon />}
+                            variant="outline"
+                          >
+                            {repTitle}{" "}
+                          </MenuButton>
+                          <MenuList>
+                            <MenuItem onClick={() => setRepTitle("Reps")}>
+                              Reps
+                            </MenuItem>
+                            <MenuItem onClick={() => setRepTitle("Min")}>
+                              Min
+                            </MenuItem>
+                            <MenuItem onClick={() => setRepTitle("Sec")}>
+                              Sec
+                            </MenuItem>
+                            <MenuItem onClick={() => setRepTitle("Laps")}>
+                              Laps
+                            </MenuItem>
+                          </MenuList>
+                        </Menu>
+                      </Box>
+                    </Stack>
+                  </FormControl>
+                </Box>
+                <Box w={{ base: "50%", md: "45%" }}>
+                  <FormControl>
+                    {/* <FormLabel>{unitTitle}</FormLabel> */}
+                    <Stack direction="row" alignItems="center" spacing="1">
+                      <Input
+                        name="units"
+                        type="number"
+                        value={units}
+                        onChange={(e) => setUnits(Number(e.target.value))}
+                      />
+                      <Box>
+                        <Menu>
+                          <MenuButton
+                            as={Button}
+                            rightIcon={<ChevronDownIcon />}
+                            variant="outline"
+                          >
+                            {unitTitle}
+                          </MenuButton>
+                          <MenuList>
+                            <MenuItem onClick={() => setUnitTitle("lbs")}>
+                              lbs
+                            </MenuItem>
+                            <MenuItem onClick={() => setUnitTitle("Intesity")}>
+                              Intesity
+                            </MenuItem>
+                            <MenuItem onClick={() => setUnitTitle("None")}>
+                              None
+                            </MenuItem>
+                          </MenuList>
+                        </Menu>
+                      </Box>
+                    </Stack>
+                  </FormControl>
+                </Box>
+              </Stack>
+              <Flex
+                h="100%"
+                w={{ base: "100%", sm: "100%", md: "auto" }}
+                alignItems={{ base: "center", sm: "center", md: "end" }}
+                justifyContent={{ base: "center", md: "end" }}
+                px="2"
+              >
+                <Box display={{ base: "none", md: "inherit" }}>
+                  <Button name="log" onClick={() => setSets(sets + 1)} py="2">
+                    <Icon display="block" as={AddIcon} h="40px" />
+                  </Button>
+                </Box>
+              </Flex>
+            </Stack>
+            <Stack>Log</Stack>
           </Stack>
-          <Stack>Log</Stack>
         </Stack>
-      </Stack>
-    </Card>
+      </Card>
+    </Reorder.Item>
   );
 }
